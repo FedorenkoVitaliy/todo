700415189bd836ca6b729a35965bb1863c9d580a
Sedrik666 <fedvetal@mail.ru>
2019-11-12 13:34:11 +0200

    feat(app): create first React elements and components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('hello word')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader/>
+            <SearchPanel/>
+            <TodoList/>
+        </div>
+    );
+};
+
+const AppHeader = () => {
+    return <h1>My Todo List</h1>;
+};
+
+const SearchPanel = () => {
+    return <input type="text" placeholder="search"/>;
+};
+
+const TodoList = () => {
+    return(
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>
+    )
+};
+
+ReactDOM.render(<App/>,
+    document.getElementById('root')
+);
