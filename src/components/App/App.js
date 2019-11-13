cb5c59b5844bc414a4b94b65c5c8baf0a555f962
Sedrik666 <fedvetal@mail.ru>
2019-11-14 00:59:06 +0200

    feat(app): create event onDeleted

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,38 +1,47 @@
-import React from 'react';
+import React, {Component}from 'react';
 import AppHeader from "../AppHeader";
 import ItemStatusFilter from "../ItemStatusFilter"
 import SearchPanel from "../SearchPanel";
 import ToDoList from "../ToDoList";
 import './App.css';
 
-const App = () => {
-    const todoData = [
-        {
-            id:1,
-            label: 'Drink Coffee',
-            important: false,
-        },
-        {   id:2,
-            label: 'Build Awesome App',
-            important: true,
-        },
-        {
-            id:3,
-            label: 'Have a lunch',
-            important: false,
+class App extends Component{
+    constructor(){
+        super();
+        this.state = {
+            todoData: [
+                {
+                    id:1,
+                    label: 'Drink Coffee',
+                    important: false,
+                },
+                {   id:2,
+                    label: 'Build Awesome App',
+                    important: true,
+                },
+                {
+                    id:3,
+                    label: 'Have a lunch',
+                    important: false,
+                }
+            ]
         }
-    ];
+    }
 
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
+    render(){
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3} />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+                <ToDoList todos={this.state.todoData}
+                          onDeleted={(id) => console.log(id)}
+                />
             </div>
-            <ToDoList todos={todoData}/>
-        </div>
-    );
+        );
+    }
 };
 
 export default App;
